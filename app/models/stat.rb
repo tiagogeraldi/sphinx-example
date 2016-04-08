e462eb60cac543d1702d69d8fcbdac16e5b9c43c
Tiago Andre Geraldi <virgulla@gmail.com>
2016-04-08 13:15:27 -0300

    Fixed stats counter

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -2,7 +2,7 @@ class Stat < ActiveRecord::Base
   def self.generate_stats!
     Log.recent_entries_by_ip.each do |log|
       stat = self.find_or_create_by(query: log.query)
-      stat.searches += log.total
+      stat.searches += 1
       stat.entries   = log.entries
       stat.save
     end
