6575b15a69f8e835e27b25a44aa11a268981a62a
Tiago Andre Geraldi <virgulla@gmail.com>
2016-04-08 15:19:54 -0300

    Bugfix recent entries interval

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,7 +1,7 @@
 class Log < ActiveRecord::Base
   scope :recent_entries_by_ip, -> do
     scoped = select('query, ip_address, entries')
-    scoped = scoped.where('created_at >= ?', 5.minutes.ago)
+    scoped = scoped.where('created_at >= ?', 10.minutes.ago)
     scoped = scoped.group('query, ip_address, entries')
   end
 end
